c9385e457be8accd1e67a3fbadcb25098abf0f34
mticciati <matthewticciati@gmail.com>
2016-02-21 21:02:42 -0800

    retry confirmable from scratch

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222030246) do
+ActiveRecord::Schema.define(version: 20160220212339) do
 
   create_table "job_applications", force: :cascade do |t|
     t.integer  "job_id",     limit: 4
@@ -42,13 +42,13 @@ ActiveRecord::Schema.define(version: 20160222030246) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.string   "role",                   limit: 255, default: "applicant", null: false
-    t.datetime "created_at",                                               null: false
-    t.datetime "updated_at",                                               null: false
     t.string   "confirmation_token",     limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email",      limit: 255
+    t.string   "role",                   limit: 255, default: "applicant", null: false
+    t.datetime "created_at",                                               null: false
+    t.datetime "updated_at",                                               null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
