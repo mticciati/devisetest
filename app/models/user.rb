1204a4a0cb9b63effd0a5a8ad984c188e22bd7f4
mticciati <matthewticciati@gmail.com>
2016-02-21 19:00:58 -0800

    adding sendgrid credentials, confirmable and timeoutable to devise config and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  #  :lockable, and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable, :timeoutable
 
    	has_many :jobs
 end
